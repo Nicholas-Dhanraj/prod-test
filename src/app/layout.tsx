ac9c8f3a747d28f3418b8b9563d40755f704cf37
nicholas-dhanraj <nickjob_dhanraj@hotmail.com>
2023-10-23 21:37:16 -0400

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,21 @@ import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
 import { Inter, Roboto, Poppins } from "next/font/google";
 import Footer from "@/components/footer/Footer";
+import react, { ReactNode } from "react";
 // import { ThemeProvider } from "@/context/ThemeContext";
 // import AuthProvider from "@/components/AuthProvider/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
-
+interface props {
+  children?: ReactNode
+  // any props that come into the component
+}
 export const metadata = {
   title: "OKC WM",
   description: "Description",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({  children, ...props }: props ) {
   return (
     <html lang="en">
       <body className={inter.className}>
