6dc7bfff538124448e8d87a0fec856e357f11e6c
nicholas-dhanraj <nickjob_dhanraj@hotmail.com>
2023-11-28 21:47:31 -0500

    'localhost'

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 async function getData() {
-  const res = await fetch("/api/posts", {
+  const res = await fetch("http://okcwm.vercel.app/api/posts", {
     cache: "no-store",
   });
 
