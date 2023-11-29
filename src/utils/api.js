cd39027f87ddcfd87b6ab2618d8a93a63c2acfc6
nicholas-dhanraj <nickjob_dhanraj@hotmail.com>
2023-11-28 22:05:38 -0500

    'localhost'

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 export const sendContactForm = async (data) =>
-  fetch("http://localhost:3000/api/contact", {
+  fetch(`https://${process.env.VERCEL_URL}/api/contact`, {
     method: "POST",
     body: JSON.stringify(data),
     headers: { "Content-Type": "application/json", Accept: "application/json" },
